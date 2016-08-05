208e654685330ea2e4a26b8cee352077abddf18a
Mark F. Ellis II <mellis@pros.com>
2016-08-05 09:29:36 -0500

    set NODE_ENV to production to get minimized version of react and redux

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,8 +12,17 @@ module.exports = {
     fs: 'empty'
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.DedupePlugin()
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
   ],
   module: {
     loaders: [{
